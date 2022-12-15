d06deadba285930ad55248c3eb81c887fb313508
haykul <halilaykul2@gmail.com>
2022-12-15 14:59:37 +0100

    init en commit

---
+++ src/Animal.java
@@ -0,0 +1,21 @@
+public class Animal {
+    public static void main(String[] args) {
+
+
+    }
+    // Overridden
+
+    public void hareket() {
+        System.out.println("Tum hayvanlar hareket eder");
+    }
+
+    public void beslenme() {
+        System.out.println("Tum hayvanlar beslenir");
+    }
+
+    public void solunum() {
+        System.out.println("Tum hayvanlar nefes alir");
+    }
+
+}
+
---
+++ src/Kuslar.java
@@ -0,0 +1,17 @@
+
+    public class Kuslar extends Animal {
+
+        public static void main(String[] args) {
+
+
+        }
+
+        public void hareket() {
+            System.out.println("Kuslar Ucarak hareket eder");
+        }
+
+
+        public void solunum() {
+            System.out.println("Kuslar akcigerleriyle nefes alir");
+        }
+}
